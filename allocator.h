951188b3be1d8410d6415a81ad917dd9c8d9c4c4
pRikh-99 <prikhodchenko.ne@gmail.com>
2026-05-26 18:49:20 +0600

    Update allocator.h

--- allocator.h
+++ allocator.h
@@ -11,8 +11,27 @@ typedef struct IAllocator {
     void* ctx;
 } IAllocator;
 
+// Заглушки
 void stub_free(IAllocator* self, void* ptr);
 void* stub_realloc(IAllocator* self, void* ptr, size_t size);
 void stub_reset(IAllocator* self);
+
+// Общие макросы или Inline-хелперы (для удобства)
+
+static inline void* i_alloc(IAllocator* a, size_t sz) {
+    return a->alloc(a, sz);
+}
+
+static inline void i_free(IAllocator* a, void* ptr) {
+    a->free(a, ptr);
+}
+
+static inline void* i_realloc(IAllocator* a, void* ptr, size_t sz) {
+    return a->realloc(a, ptr, sz);
+}
+
+static inline void i_reset(IAllocator* a) {
+    a->reset(a);
+}
 
 #endif
